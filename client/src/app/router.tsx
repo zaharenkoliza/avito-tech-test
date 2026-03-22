51de1df40415285c1860076f84149f98844a56bc
liza <Liza93087@gmail.com>
2026-03-22 15:39:49 +0300

    feat: router (remove pictures)

---
+++ client/src/app/router.tsx
@@ -0,0 +1,16 @@
+﻿import { Navigate, Route, Routes } from 'react-router-dom'
+
+import { AdDetailsPage } from '../pages/ad-details'
+import { AdEditPage } from '../pages/ad-edit'
+import { AdsListPage } from '../pages/ads-list'
+
+export const AppRouter = () => {
+	return (
+		<Routes>
+			<Route path="/ads" element={<AdsListPage />} />
+			<Route path="/ads/:id" element={<AdDetailsPage />} />
+			<Route path="/ads/:id/edit" element={<AdEditPage />} />
+			<Route path="*" element={<Navigate to="/ads" replace />} />
+		</Routes>
+	)
+}
