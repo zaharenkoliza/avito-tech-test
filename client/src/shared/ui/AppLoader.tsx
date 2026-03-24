e1ced89629f3e6f50ae96b150c71ae0bcb60c1b7
liza <Liza93087@gmail.com>
2026-03-24 18:15:13 +0300

    feat(client): loader

---
+++ client/src/shared/ui/AppLoader.tsx
@@ -0,0 +1,14 @@
+﻿import { Center, Loader } from '@mantine/core'
+
+type AppLoaderProps = {
+	minHeight?: number
+	padding?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+}
+
+export const AppLoader = ({ minHeight = 220, padding = 'xl' }: AppLoaderProps) => {
+	return (
+		<Center p={padding} mih={minHeight}>
+			<Loader type="bars" size="xl" />
+		</Center>
+	)
+}
